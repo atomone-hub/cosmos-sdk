35a55429db51cf27ca786af732d185d4f437f6b7
Julien Robert <julien@rbrt.fr>
2025-11-22 11:22:36 +0100

    compat

---
+++ x/upgrade/compat/compat.go
@@ -0,0 +1,49 @@
+package types
+
+import (
+	"context"
+
+	upgradekeeper "github.com/cosmos/cosmos-sdk/x/upgrade/keeper"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+type upgradeKeeperCompat struct {
+	*upgradekeeper.Keeper
+}
+
+func (u *upgradeKeeperCompat) GetUpgradePlan(ctx context.Context) (Plan, error) {
+	plan, err := u.Keeper.GetUpgradePlan(ctx)
+	if err != nil {
+		return Plan{}, err
+	}
+
+	return Plan{
+		Name:   plan.Name,
+		Height: plan.Height,
+		Info:   plan.Info,
+	}, nil
+}
+
+func (u *upgradeKeeperCompat) GetUpgradedClient(ctx context.Context, height int64) ([]byte, error) {
+	return u.Keeper.GetUpgradedClient(ctx, height)
+}
+
+func (u *upgradeKeeperCompat) GetUpgradedConsensusState(ctx context.Context, lastHeight int64) ([]byte, error) {
+	return u.Keeper.GetUpgradedConsensusState(ctx, lastHeight)
+}
+
+func (u *upgradeKeeperCompat) ScheduleUpgrade(ctx context.Context, plan Plan) error {
+	return u.Keeper.ScheduleUpgrade(ctx, upgradetypes.Plan{
+		Name:   plan.Name,
+		Height: plan.Height,
+		Info:   plan.Info,
+	})
+}
+
+func (u *upgradeKeeperCompat) SetUpgradedClient(ctx context.Context, planHeight int64, bz []byte) error {
+	return u.Keeper.SetUpgradedClient(ctx, planHeight, bz)
+}
+
+func (u *upgradeKeeperCompat) SetUpgradedConsensusState(ctx context.Context, planHeight int64, bz []byte) error {
+	return u.Keeper.SetUpgradedConsensusState(ctx, planHeight, bz)
+}
---
+++ x/upgrade/compat/readme.md
@@ -0,0 +1,1 @@
+This module only exists for API compability with non forked modules by AtomOne.
