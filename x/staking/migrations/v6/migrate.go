ffbd3adaf50ad294b16c58d211228d2539659c30
Pantani <danpantani@gmail.com>
2025-12-06 19:56:44 -0300

    add tests for migrations

--- x/staking/migrations/v6/migrate.go
+++ x/staking/migrations/v6/migrate.go
@@ -8,12 +8,20 @@ import (
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
+const (
+	// ModuleName is the name of the x/staking module
+	ModuleName = "staking"
+)
+
+// ParamsKey prefix for parameters for module x/staking
+var ParamsKey = []byte{0x51}
+
 // MigrateStore migrates the x/staking module state to version 6.
 func MigrateStore(ctx sdk.Context, storeService corestoretypes.KVStoreService, cdc codec.BinaryCodec) error {
 	// Open the KVStore
 	store := storeService.OpenKVStore(ctx)
 
-	paramsBz, err := store.Get(stakingtypes.ParamsKey)
+	paramsBz, err := store.Get(ParamsKey)
 	if err != nil {
 		return err
 	}
@@ -31,5 +39,5 @@ func MigrateStore(ctx sdk.Context, storeService corestoretypes.KVStoreService, c
 		return err
 	}
 
-	return store.Set(stakingtypes.ParamsKey, bz)
+	return store.Set(ParamsKey, bz)
 }
