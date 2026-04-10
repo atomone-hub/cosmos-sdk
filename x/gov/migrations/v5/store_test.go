17729873c54e15b0c3355350c40bed3b7ecb04cf
Danilo Pantani <danpantani@gmail.com>
2026-04-10 10:40:00 -0300

    fix(x/gov): fix migration clearing deprecated (A-31) (#69)
    * fix(x/gov): fix migration clearing deprecated (A-31)
    
    * add migration tests
    
    * add changelog
    
    * Update CHANGELOG.md
    
    ---------
    
    Co-authored-by: julienrbrt <julien@rbrt.fr>

--- x/gov/migrations/v5/store_test.go
+++ x/gov/migrations/v5/store_test.go
@@ -2,6 +2,7 @@ package v5_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -31,10 +32,17 @@ func TestMigrateStore(t *testing.T) {
 	constitutionAmendmentParticipationEMACollection := collections.NewItem(sb, v5.ConstitutionAmendmentParticipationEMAKey, "constitution_amendment_participation_ema", sdk.LegacyDecValue)
 	lawParticipationEMACollection := collections.NewItem(sb, v5.LawParticipationEMAKey, "law_participation_ema", sdk.LegacyDecValue)
 
+	legacyParams := legacyV4Params()
+	store.Set(v5.ParamsKey, cdc.MustMarshal(&legacyParams))
+
 	var params v1.Params
 	bz := store.Get(v5.ParamsKey)
 	require.NoError(t, cdc.Unmarshal(bz, &params))
-	require.NotNil(t, params)
+	require.Equal(t, legacyParams.MinDeposit, params.MinDeposit)
+	require.Equal(t, legacyParams.Quorum, params.Quorum)
+	require.Equal(t, legacyParams.ConstitutionAmendmentQuorum, params.ConstitutionAmendmentQuorum)
+	require.Equal(t, legacyParams.LawQuorum, params.LawQuorum)
+	require.Equal(t, legacyParams.MinInitialDepositRatio, params.MinInitialDepositRatio)
 
 	// Run migrations.
 	err := v5.MigrateStore(ctx, storeService, cdc, constitutionCollection, participationEMACollection, constitutionAmendmentParticipationEMACollection, lawParticipationEMACollection)
@@ -43,8 +51,20 @@ func TestMigrateStore(t *testing.T) {
 	// Check params
 	bz = store.Get(v5.ParamsKey)
 	require.NoError(t, cdc.Unmarshal(bz, &params))
-	require.NotNil(t, params)
+	require.Empty(t, params.MinDeposit)
+	require.Empty(t, params.Quorum)
+	require.Empty(t, params.ConstitutionAmendmentQuorum)
+	require.Empty(t, params.LawQuorum)
+	require.Empty(t, params.MinInitialDepositRatio)
 	require.Equal(t, v1.DefaultParams().MinDepositRatio, params.MinDepositRatio)
+	require.NoError(t, params.ValidateBasic())
+	require.NoError(t, v1.ValidateGenesis(v1.NewGenesisState(
+		v1.DefaultStartingProposalID,
+		v1.DefaultParticipationEma,
+		v1.DefaultParticipationEma,
+		v1.DefaultParticipationEma,
+		params,
+	)))
 
 	// Check constitution
 	result, err := constitutionCollection.Get(ctx)
@@ -64,4 +84,30 @@ func TestMigrateStore(t *testing.T) {
 	lawEMA, err := lawParticipationEMACollection.Get(ctx)
 	require.NoError(t, err)
 	require.Equal(t, expectedEMA, lawEMA)
+}
+
+func legacyV4Params() v1.Params {
+	maxDepositPeriod := v1.DefaultDepositPeriod
+	votingPeriod := v1.DefaultVotingPeriod
+
+	return v1.Params{
+		MinDeposit:                     sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, math.NewInt(10_000_000))),
+		MaxDepositPeriod:               &maxDepositPeriod,
+		VotingPeriod:                   &votingPeriod,
+		Quorum:                         "0.334000000000000000",
+		Threshold:                      v1.DefaultThreshold.String(),
+		MinInitialDepositRatio:         "0.100000000000000000",
+		BurnVoteQuorum:                 v1.DefaultBurnVoteQuorom,
+		BurnProposalDepositPrevote:     v1.DefaultBurnProposalPrevote,
+		ConstitutionAmendmentQuorum:    v1.DefaultMinConstitutionAmendmentQuorum.String(),
+		ConstitutionAmendmentThreshold: v1.DefaultConstitutionAmendmentThreshold.String(),
+		LawQuorum:                      v1.DefaultMinLawQuorum.String(),
+		LawThreshold:                   v1.DefaultLawThreshold.String(),
+		QuorumTimeout:                  durationPtr(v1.DefaultQuorumTimeout),
+		MaxVotingPeriodExtension:       durationPtr(v1.DefaultMaxVotingPeriodExtension),
+	}
+}
+
+func durationPtr(d time.Duration) *time.Duration {
+	return &d
 }
