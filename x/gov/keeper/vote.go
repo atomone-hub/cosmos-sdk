af9f61e1c1b6805f776e2eb4524c1bff7db009bd
Giuseppe Natale <12249307+giunatale@users.noreply.github.com>
2026-04-15 11:32:34 +0200

    feat(x/gov): remove `msgCancelProposal` (#85)
    * remove cancel proposal
    
    * add CL entry
    
    * update proto
    
    * fixes
    
    * remove unused
    
    * fix e2e
    
    * remove remaining traces of the cancel params

--- x/gov/keeper/vote.go
+++ x/gov/keeper/vote.go
@@ -59,14 +59,3 @@ func (keeper Keeper) AddVote(ctx context.Context, proposalID uint64, voterAddr s
 
 	return nil
 }
-
-// deleteVotes deletes all the votes from a given proposalID.
-func (keeper Keeper) deleteVotes(ctx context.Context, proposalID uint64) error {
-	rng := collections.NewPrefixedPairRange[uint64, sdk.AccAddress](proposalID)
-	err := keeper.Votes.Clear(ctx, rng)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
